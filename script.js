3c7aa90a66741575e560c4b660455b906c625338
Anamika <anamikadobriyal21@gmail.com>
2024-08-13 19:47:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,15 @@
 function fibonacci(num) {
 // your code here
-	// if(num === 1){
-	// 	return 0;
-	// }
-	// if(num === 2){
-	// 	return 1;
-	// }
+	if(num === 1){
+		return 0;
+	}
+	if(num === 2){
+		return 1;
+	}
 		
-	// if(num === 5){
-	// 	return 3;
-	// }
-	// return fibonacci(num - 1) + fibonacci(num - 2); 
-	if (num === 0) return 0;
-    if (num === 1) return 1;
-    
-    let a = 0, b = 1;
-    for (let i = 2; i <= num; i++) {
-        let next = a + b;
-        a = b;
-        b = next;
-    }
-    return b;
+	
+	return fibonacci(num - 1) + fibonacci(num - 2); 
+	
 }
 
 module.exports = fibonacci;
